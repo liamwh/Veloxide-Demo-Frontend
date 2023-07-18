490864fca073c1a873e0fc4bddf197a6625f6585
Liam Woodleigh-Hardinge <liam@liamwh.com>
2023-07-19 00:06:34 +0200

    Make navar full instead of screen, reduce Navbar title

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 </script>
 
-<div class="navbar bg-neutral w-screen">
+<div class="navbar bg-neutral w-full">
 	<div class="navbar-start" />
 	<div class="navbar-center">
-		<a href="/" class="btn btn-ghost normal-case text-xl">ExampleBanking: An example Veloxide App</a
-		>
+		<a href="/" class="btn btn-ghost normal-case text-xl">ExampleBanking</a>
 	</div>
 	<div class="navbar-end" />
 </div>
